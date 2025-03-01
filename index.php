50863a261a52f3e73fd1d07094e9102bbed38c50
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-03-02 01:51:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+// Name: Mohamed Ahmed Mohamed,
+// ID: 230104844
+
+# Assignment 1
+
+# exercise 1
+
+// $x=10;
+// $y=7;
+
+// echo "$x + $y =".($x+$y)."<br>";
+// echo "$x - $y =".($x-$y)."<br>";
+// echo "$x * $y =".($x*$y)."<br>";
+// echo "$x / $y =".($x/$y)."<br>";
+// echo "$x % $y =".($x%$y)."<br>";
+
+# exercise 2
+
+// if(date('m',time())=="8"){
+//     echo "It's August, so it's really hot.";
+// }
+// else{
+//     echo "Not August, so at least not in the peak of the heat.";
+// }
+
+# exercise 3
+
+// for($i=1;$i<=12;$i++){
+//     echo"$i * $i =".($i*$i)."<br>";
+// }
+
+# exercise 4
+
+// echo"<table border='1' cellpadding='8' cellspacing='0'>";
+// for($i=1;$i<=7;$i++){
+//     echo"<tr>";
+//     for($j=1;$j<=7;$j++){
+//         echo"<td>".$i*$j."</td>";
+//     }
+//     echo"</tr>";
+// }
+// echo"</table>";
+?>
