e94e22ee6461484de394b5c142309e1d97d39043
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-03-02 21:52:15 +0200

    Add files via upload

--- Assignment #1.php
+++ Assignment #1.php
@@ -6,39 +6,66 @@
 
 # exercise 1
 
-// $x=10;
-// $y=7;
+$x=10;
+$y=7;
 
-// echo "$x + $y =".($x+$y)."<br>";
-// echo "$x - $y =".($x-$y)."<br>";
-// echo "$x * $y =".($x*$y)."<br>";
-// echo "$x / $y =".($x/$y)."<br>";
-// echo "$x % $y =".($x%$y)."<br>";
+echo "$x + $y =".($x+$y)."<br>";
+echo "$x - $y =".($x-$y)."<br>";
+echo "$x * $y =".($x*$y)."<br>";
+echo "$x / $y =".($x/$y)."<br>";
+echo "$x % $y =".($x%$y)."<br>";
+
+echo "<br>";
+echo "<hr>";
 
 # exercise 2
 
-// if(date('m',time())=="8"){
-//     echo "It's August, so it's really hot.";
-// }
-// else{
-//     echo "Not August, so at least not in the peak of the heat.";
-// }
+if(date('F',time())==="August"){
+    echo "It's August, so it's really hot.";
+}
+else{
+    echo "Not August, so at least not in the peak of the heat.";
+}
+echo "<br>";
+echo "<hr>";
 
 # exercise 3
 
-// for($i=1;$i<=12;$i++){
-//     echo"$i * $i =".($i*$i)."<br>";
-// }
+for($i=1;$i<=12;$i++){
+    echo"$i * $i =".($i*$i)."<br>";
+}
 
-# exercise 4
-
-// echo"<table border='1' cellpadding='8' cellspacing='0'>";
-// for($i=1;$i<=7;$i++){
-//     echo"<tr>";
-//     for($j=1;$j<=7;$j++){
-//         echo"<td>".$i*$j."</td>";
-//     }
-//     echo"</tr>";
-// }
-// echo"</table>";
+echo "<br>";
+echo "<hr>";
 ?>
+
+<html>
+    <head>
+        <title>exercise 4</title>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 50%;
+            }
+            td {
+                border: 1px solid blue;
+                text-align: center;
+                padding: 8px;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Multiplication Table</h2>
+        <table>
+            <?php
+            for ($i = 1; $i <= 7; $i++) {
+                echo "<tr>";
+                for ($j = 1; $j <= 7; $j++) {
+                    echo "<td>" . ($i * $j) . "</td>";
+                }
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </body>
+</html>
